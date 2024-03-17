f77c77fde99ad18dde0dca817e6356006c3594af
JamesRezo <james@rezo.net>
2024-03-17 13:56:15 +0100

    feat: stub cache

---
+++ src/Stub/cache.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Teste si un fichier est récent (moins de n secondes).
+ *
+ * @param string $fichier Chemin du fichier
+ * @param int $n Âge testé, en secondes
+ *
+ * @return bool true si récent, false sinon
+ */
+function jeune_fichier(string $fichier, int $n): bool
+{
+	return true;
+}
--- src/stub.php
+++ src/stub.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . 'src/Stub/loading.php';
 require_once __DIR__ . 'src/Stub/logger.php';
 require_once __DIR__ . 'src/Stub/filesystem.php';
+require_once __DIR__ . 'src/Stub/cache.php';
 require_once __DIR__ . 'src/Stub/database.php';
 require_once __DIR__ . 'src/Stub/dbal.php';
 require_once __DIR__ . 'src/Stub/serializer.php';
